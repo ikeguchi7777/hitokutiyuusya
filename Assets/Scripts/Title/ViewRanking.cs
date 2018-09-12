60683241b2898922ea6bb12645f72bb88e18f533
ikeguchi7777 <ikeguchi.0723@gmail.com>
2018-09-12 17:11:48 +0900

    Rankingを追加
    未完成だがRankingの表示機能を追加

---
+++ Assets/Scripts/Title/ViewRanking.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewRanking : MonoBehaviour {
+    [SerializeField] ScorePanel panel;
+    private void Awake()
+    {
+        Ranking.Instance.Load();
+        foreach (var item in Ranking.Instance.ScoreRanking)
+        {
+            panel.Set(item.name, item.score);
+            Instantiate(panel, transform);
+        }
+    }
+}
