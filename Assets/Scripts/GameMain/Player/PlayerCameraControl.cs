b7173650c4aa6a35021c6a2b8e7b93b48c34db4e
ikeguchi7777 <ikeguchi.0723@gmail.com>
2018-09-23 14:19:38 +0900

    カメラ　ロックオンをシームレスに

--- Assets/Scripts/GameMain/Player/PlayerCameraControl.cs
+++ Assets/Scripts/GameMain/Player/PlayerCameraControl.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public enum CameraState
 {
     Default,
-    LockOn
+    LockOn,
+    ToLockOn,
+    FromLockOn
 }
 public class PlayerCameraControl : StatefulObjectBase<PlayerCameraControl, CameraState>
 {
@@ -33,6 +37,8 @@ public class PlayerCameraControl : StatefulObjectBase<PlayerCameraControl, Camer
     {
         stateList.Add(new StateDefault(this));
         stateList.Add(new StateLockOn(this));
+        stateList.Add(new StateToLockOn(this));
+        stateList.Add(new StateFromLockOn(this));
     }
 
     class StateDefault : State<PlayerCameraControl>
@@ -46,12 +52,77 @@ public class PlayerCameraControl : StatefulObjectBase<PlayerCameraControl, Camer
 
         public override void Execute()
         {
-            owner.Yaw %= 360;
+            if (owner.Yaw > 180)
+                owner.Yaw -= 360;
+            else if (owner.Yaw < -180)
+                owner.Yaw += 360;
             var dir = Quaternion.Euler(owner.Pitch, owner.Yaw, 0.0f) * Vector3.back * owner.Distance;
             owner.transform.position = owner.player.position + dir;
             owner.transform.LookAt(owner.player);
         }
     }
+
+    class StateToLockOn : State<PlayerCameraControl>
+    {
+        Quaternion to;
+        Tweener tweener, d_tweener;
+        public StateToLockOn(PlayerCameraControl owner) : base(owner, CameraState.ToLockOn) { }
+
+        public override void Enter()
+        {
+            to = Quaternion.LookRotation(owner.LockOnTransform.position - owner.transform.position);
+            tweener = owner.transform.DORotateQuaternion(to, 0.5f);
+            d_tweener = DOTween.To(() => owner.Yaw, num => owner.Yaw = num, GetYaw(), 0.5f);
+        }
+
+        public override void Execute()
+        {
+            to = Quaternion.LookRotation(owner.LockOnTransform.position - owner.transform.position);
+            var dir = Quaternion.Euler(owner.Pitch, owner.Yaw, 0.0f) * Vector3.back * owner.Distance;
+            owner.transform.position = owner.player.position + dir;
+            Debug.Log(owner.Yaw);
+            if (tweener.position >= 0.5f)
+                owner.ChangeState(CameraState.LockOn);
+            else
+            {
+                var t = tweener.position + Time.deltaTime;
+                tweener.ChangeEndValue(to).Goto(t);
+                d_tweener.ChangeEndValue(GetYaw()).Goto(t);
+            }
+        }
+
+        float GetYaw()
+        {
+            var dir = (owner.player.position - owner.LockOnTransform.position);
+            dir = Vector3.Scale(dir, new Vector3(1, 0, 1)).normalized;
+            var angle = Vector3.Angle(Vector3.back, dir) * (dir.x < 0 ? 1 : -1);
+            if (angle * owner.Yaw < -180)
+            {
+                if (angle < 0)
+                    angle += 360;
+                else
+                    angle -= 360;
+            }
+            return angle;
+        }
+
+        public override void Exit()
+        {
+            tweener.Kill();
+            d_tweener.Kill();
+        }
+    }
+
+    class StateFromLockOn : State<PlayerCameraControl>
+    {
+        public StateFromLockOn(PlayerCameraControl owner) : base(owner, CameraState.FromLockOn) { }
+
+        public override void Enter()
+        {
+            owner.ChangeState(CameraState.Default);
+        }
+    }
+
     class StateLockOn : State<PlayerCameraControl>
     {
         public StateLockOn(PlayerCameraControl owner) : base(owner, CameraState.LockOn) { }
