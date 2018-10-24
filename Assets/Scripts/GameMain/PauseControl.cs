ab8632c1f25039938761361467bfede5877be0cc
ikeguchi7777 <ikeguchi.0723@gmail.com>
2018-10-25 00:38:25 +0900

    ポーズが出来るように変更

--- Assets/Scripts/GameMain/PauseControl.cs
+++ Assets/Scripts/GameMain/PauseControl.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PauseControl : MonoBehaviour {
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] GameObject firstSelected;
     EventSystem eventSystem;
+    StandaloneInputModule inputModule;
 
     private void Awake()
     {
         eventSystem = GetComponent<EventSystem>();
+        inputModule = GetComponent<StandaloneInputModule>();
     }
 
     public void Pause(int i)
     {
         Time.timeScale = 0.0f;
+        SetButton(i);
+        pausePanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(firstSelected);
     }
 
     public void Resume()
@@ -23,13 +30,18 @@ public class PauseControl : MonoBehaviour {
 
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(0.5f);
+        pausePanel.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
         Time.timeScale = 1.0f;
         gameObject.SendMessage("ChangeState", GameState.Playing);
     }
 
     void SetButton(int i)
     {
-
+        var input = PlayerInput.PlayerInputs[i];
+        inputModule.horizontalAxis = input.GetAxisName(EAxis.X);
+        inputModule.verticalAxis = input.GetAxisName(EAxis.Y);
+        inputModule.submitButton = input.GetButtonName(EButton.WeakAttackAndSubmit);
+        inputModule.cancelButton = input.GetButtonName(EButton.Evade);
     }
 }
