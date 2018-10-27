344d61002a070b70942e40c041a784d960c6563a
ikeguchi7777 <ikeguchi.0723@gmail.com>
2018-10-27 23:27:10 +0900

    ボス追加

--- Assets/Scripts/GameMain/Enemy/BossControl.cs
+++ Assets/Scripts/GameMain/Enemy/BossControl.cs
@@ -7,14 +7,28 @@ public enum BossState
 {
     Wait,
     Idle,
-    CallEnemy,
-    Breath,
-    WeakAttack,
-    StrongAttack,
-    SpecialAttack
+    WalkToPlayer,
+    WalkToPlayerJump,
+    Attack
 }
+
 public class BossControl : EnemyControl<BossControl, BossState>
 {
+    enum BossAttackState
+    {
+        None,
+        Punch_R,
+        Punch_L,
+        Shout,
+        Combo,
+        Bleath,
+        RotationAttack,
+        JumpAttack,
+        Magic
+    }
+
+    readonly float[] interval = { 0.0f, 0.5f, 0.5f, 0.2f, 1.0f, 1.0f, 1.0f, 0.2f, 3.0f };
+    BossAttackState attack = BossAttackState.None;
     protected override BossState GetFirstState()
     {
         return BossState.Wait;
@@ -28,7 +42,7 @@ public class BossControl : EnemyControl<BossControl, BossState>
 
     protected override void Wince()
     {
-        
+
     }
 
     class StateWait : State<BossControl>
@@ -45,7 +59,49 @@ public class BossControl : EnemyControl<BossControl, BossState>
         }
         public override void Enter()
         {
-            
+
         }
+    }
+
+    class StateWalkToPlayer : State<BossControl>
+    {
+        public StateWalkToPlayer(BossControl owner) : base(owner, BossState.WalkToPlayer)
+        {
+        }
+    }
+
+    class StateWalkToPlayerJump : State<BossControl>
+    {
+        public StateWalkToPlayerJump(BossControl owner) : base(owner, BossState.WalkToPlayerJump)
+        {
+        }
+    }
+
+    class StateAttack : State<BossControl>
+    {
+        float time;
+        public StateAttack(BossControl owner) : base(owner, BossState.Attack)
+        {
+        }
+
+        public override void Enter()
+        {
+            time = 0.0f;
+        }
+
+        public override void Execute()
+        {
+            time += Time.deltaTime;
+            if (time < owner.interval[(int)owner.attack])
+                return;
+            owner.animator.SetInteger("AttackType", (int)owner.attack);
+            owner.animator.SetTrigger("Attack");
+        }
+    }
+
+    private void OnAnimatorMove()
+    {
+        transform.position += animator.deltaPosition * 100;
+        transform.rotation = animator.rootRotation;
     }
 }
