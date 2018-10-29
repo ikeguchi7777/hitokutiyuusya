dcf438f196a8ecba51effc4a0d430c985f7f384f
ikeguchi7777 <ikeguchi.0723@gmail.com>
2018-10-29 10:24:54 +0900

    パーティクルを追加　ボスの当たり判定作成

---
+++ Assets/Scripts/GameMain/Enemy/Boss/BossActionControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossActionControl : MonoBehaviour
+{
+    [SerializeField] ParticleSystem breath;
+    [SerializeField] Collider r_hand, l_hand, hip;
+
+    public void SetBreath(int isActive)
+    {
+        if (isActive == 1)
+            breath.Play(true);
+        else
+            breath.Stop(true);
+    }
+
+    public void SetRHand(int isActive)
+    {
+        r_hand.enabled = isActive == 1;
+    }
+
+    public void SetLHand(int isActive)
+    {
+        l_hand.enabled = isActive == 1;
+    }
+
+    public void SetHip(int isActive)
+    {
+        hip.enabled = isActive == 1;
+    }
+}
